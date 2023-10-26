e718f50ec9e2b40a9b34c153de774c283bd89982
eleanorewu <yuenwu850823@gmail.com>
2023-10-26 14:54:18 +0800

    feat: loading component

---
+++ components/Loading.vue
@@ -0,0 +1,66 @@
+<template>
+  <div class="loading-indicator">
+    <img
+      class="avatar"
+      src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTJlE7FjHztLrji72AKd5EvLnj21j8YdicZOFhzJQhadw&s"
+      alt=""
+    />
+    <div class="msg-box">
+      <span class="msg" :key="msgContent">{{ msgContent }}</span>
+      <span>選一隻會做事的貓咪...</span>
+    </div>
+  </div>
+</template>
+
+<script setup>
+import { ref } from "vue";
+
+const msgContent = ref("不投我的都是狗");
+</script>
+
+<style scoped>
+.loading-indicator {
+  display: flex;
+  justify-content: center;
+  align-items: flex-start;
+  animation: openin 1s ease-in-out;
+  gap: 0.5rem;
+  margin: 1rem auto;
+  & img {
+    width: 4.5rem;
+    height: 4.5rem;
+    object-fit: cover;
+    border-radius: 4.5rem;
+    overflow: hidden;
+    border: 1px solid var(--border-color);
+  }
+  & .msg-box {
+    display: flex;
+    flex-direction: column;
+    align-items: flex-start;
+    & .msg {
+      font-size: 1.875rem;
+      padding: 0.75rem 1.5rem;
+      color: var(--color-text);
+      background-color: var(--bg);
+      border: 1px solid var(--border-color);
+      border-radius: 0.125rem 2rem 2rem;
+      margin-bottom: 0.25rem;
+    }
+    & span {
+      padding: 0 1rem;
+    }
+  }
+}
+
+@keyframes openin {
+  0% {
+    opacity: 0;
+    transform: translateY(30px);
+  }
+  100% {
+    opacity: 1;
+    transform: translateY(0px);
+  }
+}
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,89 +1,29 @@
 <template>
-  <div>
-    <button>
-      <font-awesome-icon icon="fa-solid fa-circle-half-stroke" />主題模式
-      {{ $colorMode.value }}
-    </button>
-    <div class="msg-box">
-      <img
-        class="avatar"
-        src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTJlE7FjHztLrji72AKd5EvLnj21j8YdicZOFhzJQhadw&s"
-        alt=""
-      />
-      <p class="msg" :key="msgContent">{{ msgContent }}</p>
-    </div>
+  <div class="page">
+    <Loading />
     <select v-model="$colorMode.preference">
       <option value="system">System</option>
       <option value="light">Light</option>
       <option value="dark">Dark</option>
     </select>
-    <ul>
-      <li><NuxtLink to="/about" title="關於我們">關於我們</NuxtLink></li>
-      <li>
-        <NuxtLink
-          to="https://store.line.me/stickershop/product/24123123/zh-Hant"
-          title="另開新視窗 前往line貼圖"
-          target="_blank"
-          external
-          >line貼圖</NuxtLink
-        >
-      </li>
-    </ul>
     <Card />
-    <Card />
-    <Card />
-    <Card />
+    <!-- <NuxtLink
+      to="https://store.line.me/stickershop/product/24123123/zh-Hant"
+      title="另開新視窗 前往line貼圖"
+      target="_blank"
+      external
+      >line貼圖</NuxtLink
+    > -->
   </div>
 </template>
 
 <script setup>
-import { ref } from "vue";
 const colorMode = useColorMode();
 console.log(colorMode.preference);
-
-const msgContent = ref("2024");
-setTimeout(() => {
-  msgContent.value = "就靠貓咪拯救世界";
-}, 1500);
 </script>
 
 <style scoped>
 h1 {
   color: var(--color-primary);
-}
-.msg-box {
-  display: flex;
-  justify-content: center;
-  align-items: flex-end;
-  animation: openin 1s ease-in-out;
-  gap: 0.5rem;
-  margin: 1rem;
-  & img {
-    width: 4rem;
-    height: 4rem;
-    object-fit: cover;
-    border-radius: 4rem;
-    overflow: hidden;
-    border: 1px solid var(--border-color);
-  }
-  & .msg {
-    font-size: 1.75rem;
-    padding: 0.75rem 1rem;
-    color: var(--color-text);
-    background-color: var(--bg);
-    border: 1px solid var(--border-color);
-    border-radius: 0.125rem 2rem 2rem;
-  }
-}
-
-@keyframes openin {
-  0% {
-    opacity: 0;
-    transform: translateY(10px);
-  }
-  100% {
-    opacity: 1;
-    transform: translateY(0px);
-  }
 }
 </style>
