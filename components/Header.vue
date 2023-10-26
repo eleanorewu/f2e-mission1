9acb0dcde8061189ea8a87b673d9f7c56f502ab3
Laura <laura@blueplanet.com.tw>
2023-10-26 16:39:17 +0800

    fix: remove header colormode

--- components/Header.vue
+++ components/Header.vue
@@ -1,129 +1,113 @@
 <template>
   <div class="header">
-    <p>Header</p>
-    <input
-      type="checkbox"
-      id="colormode-switch"
-      @click="swtichMode('switch')"
-    />
-    <label for="colormode-switch" class="color-mode-switch">
-      <SvgIconDark class="icon-dark"></SvgIconDark>
-      <SvgIconLight class="icon-light"></SvgIconLight>
-    </label>
+    <div class="navagationbar">
+      <img
+        class="logo"
+        src="https://d2h0caecvn34a3.cloudfront.net/prod/2021/06/HeroMAMA-logo_工作區域-1.png"
+        alt="這是logo"
+      />
+      <ul class="main-menu">
+        <li>認識喵星人</li>
+        <li>喵的未來</li>
+        <li>喵的足跡</li>
+        <li>支持本喵</li>
+        <li>喵電感應</li>
+      </ul>
+      <ul class="action-menu">
+        <li>
+          <select class="no-border">
+            <option value="Zh">中文</option>
+            <option value="Meow">喵語</option>
+          </select>
+        </li>
+        <li><ColorMode /></li>
+      </ul>
+    </div>
+    <ul class="mobile-menu">
+      <li>認識喵星人</li>
+      <li>喵的未來</li>
+      <li>喵的足跡</li>
+      <li>支持本喵</li>
+      <li>喵電感應</li>
+    </ul>
   </div>
 </template>
 
-<script setup>
-import SvgIconDark from "@/components/SvgIcon/Dark.vue";
-import SvgIconLight from "@/components/SvgIcon/Light.vue";
-const colorMode = useColorMode();
-const darkMode = () => {
-  colorMode.preference = "dark";
-  document.getElementById("colormode-switch").checked = true;
-};
-const lightMode = () => {
-  colorMode.preference = "light";
-  document.getElementById("colormode-switch").checked = false;
-};
-console.log("setup", colorMode);
-const swtichMode = (mode) => {
-  let preference = colorMode.preference;
-  let value = colorMode.value;
-
-  switch (mode) {
-    case undefined:
-      if (preference === "system" && value === "light") {
-        lightMode();
-      } else if (preference === "system" && value === "dark") {
-        darkMode();
-      } else if (preference === "dark" && value === "dark") {
-        darkMode();
-      } else {
-        lightMode();
-      }
-      break;
-    case "switch":
-      console.log("switch");
-      if (preference === "dark" && value === "dark") {
-        lightMode();
-      } else {
-        darkMode();
-      }
-      break;
-  }
-};
-onMounted(() => {
-  swtichMode();
-});
-</script>
+<script setup></script>
 
 <style scoped>
 .header {
   width: 100%;
-  height: 80px;
-  display: flex;
-  justify-content: center;
-  align-items: center;
+  height: auto;
+  position: fixed;
+  top: 0;
+  padding: 0.25rem 1rem;
   background-color: var(--bg);
   color: var(--color-text);
   border-bottom: 1px solid var(--border-color);
-  & p {
-    padding: 1rem;
+}
+
+.navagationbar {
+  max-width: 1440px;
+  margin: 0 auto;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  position: relative;
+  & .logo {
+    width: 120px;
+    height: 52px;
+    object-fit: contain;
+  }
+  & .main-menu {
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    font-size: 1.125rem;
+    transform: translateX(-50%) translateY(-50%);
+    @media screen and (width <= 1080px) {
+      display: none;
+    }
+    & li {
+      padding: 0.25rem;
+      display: inline;
+      cursor: pointer;
+      white-space: nowrap;
+    }
+  }
+  & .action-menu {
+    display: flex;
+    align-items: center;
+    & li {
+      padding: 0;
+      margin-left: 0.5rem;
+      & .no-border {
+        border: none;
+      }
+    }
   }
 }
-input[type="checkbox"] {
-  height: 0;
-  width: 0;
-  visibility: hidden;
-}
-label {
-  cursor: pointer;
-  width: 70px;
-  height: 35px;
-  background: #aeaeae;
-  display: block;
-  border-radius: 6.25rem;
-  position: relative;
-}
-label:after {
-  content: "";
-  position: absolute;
-  top: 5px;
-  left: 5px;
-  width: 25px;
-  height: 25px;
-  background-color: #fff;
-  border-radius: 5.75rem;
-  transition: 0.3s;
-}
-.color-mode-switch::before,
-.color-mode-switch::after {
-  display: block;
-  color: #fff;
-  box-sizing: border-box;
-}
-
-.color-mode-switch {
-  position: relative;
-}
-.icon-dark {
-  position: absolute;
-  left: 10px;
-  top: 6px;
-  z-index: 10;
-}
-.icon-light {
-  position: absolute;
-  right: 10px;
-  top: 7px;
-  z-index: 10;
-}
-
-input:checked + label {
-  background: #3a3a3a;
-}
-input:checked + label:after {
-  left: calc(100% - 5px);
-  transform: translateX(-100%);
+.mobile-menu {
+  width: 100%;
+  padding: 0.25rem 0;
+  display: none;
+  font-size: 1.125rem;
+  @media screen and (width <= 1080px) {
+    display: flex;
+    flex-wrap: nowrap;
+    justify-content: center;
+    overflow-x: scroll;
+    &::-webkit-scrollbar {
+      display: none;
+    }
+  }
+  @media screen and (width <= 480px) {
+    justify-content: start;
+  }
+  & li {
+    white-space: nowrap;
+    padding: 0.25rem;
+    cursor: pointer;
+  }
 }
 </style>
