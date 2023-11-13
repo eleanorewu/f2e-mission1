35d075237954d9c1164c06f0d8ed8263d4269d57
eleanorewu <yuenwu850823@gmail.com>
2023-11-13 19:51:01 +0800

    修改meta data跟favicon

--- pages/index.vue
+++ pages/index.vue
@@ -1,11 +1,6 @@
 <template>
   <div class="page">
     <Loading />
-    <!-- <select v-model="colorMode.preference">
-      <option value="system">System</option>
-      <option value="light">Light</option>
-      <option value="dark">Dark</option>
-    </select> -->
     <Card />
     <IntroduceBox />
     <h2>喵喵的最新軌跡</h2>
