5e9905c9566ebe4fb0895362bdc7e3da83725b5a
eleanorewu <yuenwu850823@gmail.com>
2023-11-14 11:50:18 +0800

    fix: component style

--- components/Contact/index.vue
+++ components/Contact/index.vue
@@ -1,36 +1,40 @@
 <template>
   <div class="contact">
     <Title />
-    <div class="form-container">
-      <div class="input-container">
-        <label for="user_name">Name</label>
-        <input
-          type="text"
-          name="user_name"
-          id="user_name"
-          v-model="templateParams.user_name"
-        />
+    <div class="contact-container">
+      <div class="form">
+        <div class="input-container">
+          <label for="user_name">姓名</label>
+          <input
+            type="text"
+            name="user_name"
+            id="user_name"
+            v-model="templateParams.user_name"
+            placeholder="請輸入姓名"
+          />
+        </div>
+        <div class="input-container">
+          <label for="user_mail">E-mail</label>
+          <input
+            type="text"
+            name="user_mail"
+            id="user_mail"
+            v-model="templateParams.user_mail"
+            placeholder="請輸入E-mail"
+          />
+        </div>
+        <div class="input-container">
+          <label for="user_message">內容</label>
+          <textarea
+            name="user_message"
+            id="user_message"
+            v-model="templateParams.user_message"
+            rows="5"
+            placeholder="請輸入內容"
+          ></textarea>
+        </div>
+        <button @click="sendEmail()">送出</button>
       </div>
-      <div class="input-container">
-        <label for="user_mail">Email</label>
-        <input
-          type="text"
-          name="user_mail"
-          id="user_mail"
-          v-model="templateParams.user_mail"
-        />
-      </div>
-      <div class="input-container">
-        <label for="user_message">Message</label>
-        <textarea
-          name="user_message"
-          id="user_message"
-          v-model="templateParams.user_message"
-          rows="5"
-          cols="30"
-        ></textarea>
-      </div>
-      <button @click="sendEmail()">送出</button>
     </div>
   </div>
 </template>
@@ -68,22 +72,47 @@ const sendEmail = () => {
   width: 100%;
   height: auto;
 }
-.form-container {
-  margin: 4rem 18rem 10rem;
-  /* text-align: center; */
-}
-.input-container {
-  padding: 0.75rem;
-  & label {
-    padding-right: 0.5rem;
+.contact-container {
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: flex-start;
+  & .form {
+    width: 50%;
+    display: flex;
+    flex-direction: column;
+    justify-content: flex-start;
+    align-items: flex-start;
+    & .input-container {
+      width: 100%;
+      padding: 0.75rem;
+      display: flex;
+      flex-direction: column;
+      justify-content: flex-start;
+      align-items: flex-start;
+      & label {
+        width: 100%;
+        text-align: left;
+        font-size: 1.25rem;
+        font-weight: bold;
+        margin-bottom: 0.5rem;
+      }
+      & input {
+        width: 100%;
+        height: 3rem;
+        text-align: left;
+        padding: 0.5rem 1rem;
+        border-radius: 0.75rem;
+        background-color: var(--color-white);
+      }
+      & textarea {
+        width: 100%;
+        text-align: left;
+        padding: 0.5rem 1rem;
+        border-radius: 0.75rem;
+        background-color: var(--color-white);
+      }
+    }
   }
-  & input {
-    min-width: 15rem;
-  }
-}
-input,
-textarea {
-  border: 1px solid #8d8d8d;
-  border-radius: 0.25rem;
 }
 </style>
