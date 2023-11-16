90be2129e24b32a3575dd618a75e4da8c467760a
Laura <laura@blueplanet.com.tw>
2023-11-16 14:55:25 +0800

    feat: update introduce block i18n

--- components/Introduce/Masonry.vue
+++ components/Introduce/Masonry.vue
@@ -5,130 +5,144 @@
         <img class="item-img" src="~/assets/images/avatar.png" alt="喵立翰" />
       </div>
       <div>
-        <h4>喵立翰</h4>
+        <h4>{{ $t("introduces.in[0]") }}</h4>
         <p class="item-content">
-          三歲已結紮公貓<br />
-          隸屬 勸世貓黨<br />
-          MBTI: ENFJ<br />
-          不分區立委候選人1號<br />
-          即使什麼都不做還是很可愛
+          {{ $t("introduces.in[1]") }}<br />
+          {{ $t("introduces.in[2]") }}<br />
+          {{ $t("introduces.in[3]") }}<br />
+          {{ $t("introduces.in[4]") }}<br />
+          {{ $t("introduces.in[5]") }}
         </p>
-        <p class="item-content">#迷因貓貓 #無障礙挑戰 #珍奶殺手</p>
+        <p class="item-content">{{ $t("introduces.in[6]") }}</p>
       </div>
     </div>
     <div class="item item2">
-      <h4>喵的經歷</h4>
+      <h4>{{ $t("introduces.ex[0]") }}</h4>
       <p class="item-content">
-        人類馴化師<br />
-        畢業於青椒田大學<br />
-        曾出演百老匯音樂-貓<br />
-        門薩會員<br />
-        貓咪障礙賽冠軍<br />
-        擁有無數個貓奴
+        {{ $t("introduces.ex[1]") }}<br />
+        {{ $t("introduces.ex[2]") }}<br />
+        {{ $t("introduces.ex[3]") }}<br />
+        {{ $t("introduces.ex[4]") }}<br />
+        {{ $t("introduces.ex[5]") }}<br />
+        {{ $t("introduces.ex[6]") }}
       </p>
     </div>
     <div class="item item3">
-      <h4>喵最愛看</h4>
+      <h4>{{ $t("introduces.me[0]") }}</h4>
       <p class="item-content">
         <b>
-          <a href="https://www.youtube.com/channel/UCypzlOdJyyOR2NhKv0o3zig"
-            >動物星球頻道</a
-          ></b
+          <a href="https://www.youtube.com/channel/UCypzlOdJyyOR2NhKv0o3zig">{{
+            $t("introduces.me[1].title")
+          }}</a></b
         ><br />
-        動物星球頻道是全球唯一的動物紀實娛樂頻道，有新奇、冒險、歡笑、動人情感、還有生與死，以熱情感性的深度內容，為人類與動物搭起橋樑。
+        {{ $t("introduces.me[1].content") }}
       </p>
       <p class="item-content">
         <b
-          ><a href="https://www.youtube.com/@fumeancats">皇阿瑪的後宮生活</a></b
+          ><a href="https://www.youtube.com/@fumeancats">{{
+            $t("introduces.me[2].title")
+          }}</a></b
         >
         <br />
-        由台灣 YouTuber 志銘與狸貓兩位自稱貓奴的飼主，於2015年創辦的YouTube
-        頻道。內容主要紀錄八隻貓咪的生活，受許多愛貓人士的喜愛。
+        {{ $t("introduces.me[2].content") }}
       </p>
       <p class="item-content">
-        <b><a href="https://www.youtube.com/@LadyFlavor">好味小姐</a></b
+        <b
+          ><a href="https://www.youtube.com/@LadyFlavor">{{
+            $t("introduces.me[3].title")
+          }}</a></b
         ><br />
-        創建了以貓咪日常和貓食製作為主題的 YouTube 頻道「好味小姐」
+        {{ $t("introduces.me[3].content") }}
       </p>
     </div>
     <div class="item item4">
-      <h4>競選總部</h4>
-      <p class="item-content">喵星路9487巷87號空紙箱</p>
+      <h4>{{ $t("introduces.ad[0]") }}</h4>
+      <p class="item-content">{{ $t("introduces.ad[1]") }}</p>
       <img class="item-img" src="~/assets/images/box.png" alt="空紙箱" />
     </div>
     <div class="item item5">
-      <h4>喵的主題曲</h4>
+      <h4>{{ $t("introduces.mu[0]") }}</h4>
       <p class="item-content">
-        勸世寶貝-喵電感應<br />
-        林強-向前走<br />
-        葉啟田-愛拼才會贏<br />
-        黃妃-追追追<br />
-        滅火器-長途夜車<br />
+        {{ $t("introduces.mu[1]") }}<br />
+        {{ $t("introduces.mu[2]") }}<br />
+        {{ $t("introduces.mu[3]") }}<br />
+        {{ $t("introduces.mu[4]") }}<br />
+        {{ $t("introduces.mu[5]") }}<br />
       </p>
       <b>
         <a
           href="https://www.youtube.com/playlist?list=PLtClt7-lTL9-S_IQu9-x4aUX0pCByqY2j"
-          >前往完整播放清單</a
+          >{{ $t("introduces.mu[6]") }}</a
         ></b
       >
     </div>
     <div class="item item6">
-      <h4>喵的愛牌</h4>
+      <h4>{{ $t("introduces.br[0]") }}</h4>
       <p class="item-content">
-        <b><a href="https://piman.cc/">Piman</a></b
+        <b
+          ><a href="https://piman.cc/">{{ $t("introduces.br[1].title") }}</a></b
         ><br />
-        喵的嚴選品牌，致力打造網路無障礙空間而誕生的ＵＩ框架。
+        {{ $t("introduces.br[1].content") }}
       </p>
       <p class="item-content">
-        <b><a href="https://www.instagram.com/piman_a11y/">社群</a></b
+        <b
+          ><a href="https://www.instagram.com/piman_a11y/">{{
+            $t("introduces.br[2].title")
+          }}</a></b
         ><br />
-        一起來看看 Ｐiman 都在 ig 做什麼。
+        {{ $t("introduces.br[2].content") }}
       </p>
       <p class="item-content">
-        <b><a href="https://kusdom.com/piman/zh_tw">周邊商品</a></b
+        <b
+          ><a href="https://kusdom.com/piman/zh_tw">{{
+            $t("introduces.br[3].title")
+          }}</a></b
         ><br />
-        佛要金裝，貓要衣裝.... 最潮貓貓的衣著配件在這裡
+        {{ $t("introduces.br[3].content") }}
       </p>
     </div>
     <div class="item item7">
-      <h4>喵的盟友</h4>
+      <h4>{{ $t("introduces.fr[0]") }}</h4>
       <p class="item-content">
         <b
-          ><a href="https://ithelp.ithome.com.tw/users/20152260/ironman/5614"
-            >無障礙大叔</a
-          ></b
+          ><a href="https://ithelp.ithome.com.tw/users/20152260/ironman/5614">{{
+            $t("introduces.fr[1]")
+          }}</a></b
         ><br />
-        本喵在青椒田大學就讀時的大大前輩-無障礙大叔，發表了一系列無障礙挑戰賽文章，看完讓你人生從此通行無阻。
+        {{ $t("introduces.fr[2]") }}
       </p>
     </div>
     <div class="item item8">
-      <h4>請喵喝珍奶</h4>
+      <h4>{{ $t("introduces.te[0]") }}</h4>
       <p class="item-content">
-        本喵的口有點渴<br />
-        請我喝一杯珍奶吧
+        {{ $t("introduces.te[1]") }}<br />
+        {{ $t("introduces.te[2]") }}
       </p>
       <b
         ><a
           href="https://neillin.bobaboba.me/?source=post_page-----c7466b1faf76--------------------------------"
-          >買珍奶給喵喵</a
+          >{{ $t("introduces.te[3]") }}</a
         ></b
       >
     </div>
     <div class="item item9">
-      <h4>喵的同盟</h4>
+      <h4>{{ $t("introduces.al[0]") }}</h4>
       <p class="item-content">
-        <b><a href="https://www.facebook.com/CatPartyTaiwan/">台灣貓黨</a></b
+        <b
+          ><a href="https://www.facebook.com/CatPartyTaiwan/">{{
+            $t("introduces.al[1]")
+          }}</a></b
         ><br />
-        關注台灣貓黨，讓你歡樂無法黨（單押1)讓你時時刻刻被可愛籠罩
+        {{ $t("introduces.al[2]") }}
       </p>
     </div>
     <div class="item item10">
-      <h4>喵生再走，貼圖要有</h4>
-      <p class="item-content">你的心聲由貓咪來幫你訴說...<br /></p>
+      <h4>{{ $t("introduces.st[0]") }}</h4>
+      <p class="item-content">{{ $t("introduces.st[1]") }}<br /></p>
       <b
-        ><a href="https://store.line.me/stickershop/product/24123123/zh-Hant"
-          >點此前往購買Line貼圖</a
-        ></b
+        ><a href="https://store.line.me/stickershop/product/24123123/zh-Hant">{{
+          $t("introduces.st[2]")
+        }}</a></b
       >
     </div>
   </div>
