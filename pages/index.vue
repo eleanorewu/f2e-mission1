501012083696ee49e3938cb5a3138c4ad22e7407
eleanorewu <yuenwu850823@gmail.com>
2023-12-01 10:22:00 +0800

    timeline 樣式修改

--- pages/index.vue
+++ pages/index.vue
@@ -6,11 +6,14 @@
     <!-- <IntroduceBox /> -->
     <Policy />
     <!-- <Timeline class="custom-timeline" :colorMode="colorMode" :lang="lang" /> -->
-    <div
-      class="custom-timeline"
-      id="timeline-embed"
-      style="width: 1440px; height: 650px"
-    ></div>
+    <div class="block">
+      <h4 class="block-title" id="timeline">{{ $t("nav[2]") }}</h4>
+      <div
+        class="custom-timeline"
+        id="timeline-embed"
+        style="width: 1440px; height: 650px"
+      ></div>
+    </div>
     <Sponsor />
     <Contact />
   </div>
@@ -38,46 +41,38 @@ useHead({
 const options = {
   timenav_position: "top",
 };
-onMounted(() => {
-  if (process.client) {
-    console.log(`srcId`, srcID.value);
-    window.timeline = new TL.Timeline(
-      "timeline-embed",
-      `https://docs.google.com/spreadsheets/d/${srcID.value}/pubhtml`,
-      options
-    );
-  }
-});
+
 const { preference } = useColorMode();
 const { locale } = useI18n();
-
 const colorMode = ref(preference);
-const lang = ref(locale);
-// const srcID = ref("");
 
-// const srcID = computed(() => {
-//   console.log(`lang.value`, lang.value);
-//   if (lang.value === "zhTW")
-//     return "1dR9TQG2tWyi2qUItkKAettg8ZL0_S2uOHEJFs7ShDWI";
-//   else if (lang.value == "meow")
-//     return "1yJXBeQioa_mpX7RT10jgjr0WY-_tF-kQveeYdyaxjWg";
-// });
+const srcID = ref("");
+srcID.value =
+  locale.value === "zhTW"
+    ? "1dR9TQG2tWyi2qUItkKAettg8ZL0_S2uOHEJFs7ShDWI"
+    : "1yJXBeQioa_mpX7RT10jgjr0WY-_tF-kQveeYdyaxjWg";
 
-const srcID = computed({
-  // getter
-  get() {
-    return lang.value === "zhTW"
-      ? `1dR9TQG2tWyi2qUItkKAettg8ZL0_S2uOHEJFs7ShDWI`
-      : `1yJXBeQioa_mpX7RT10jgjr0WY-_tF-kQveeYdyaxjWg`;
-  },
-  // setter
-  set(newValue) {
-    console.log(`newValue`, newValue);
-    if (lang.value === "zhTW")
-      return "1dR9TQG2tWyi2qUItkKAettg8ZL0_S2uOHEJFs7ShDWI";
-    else if (lang.value == "meow")
-      return "1yJXBeQioa_mpX7RT10jgjr0WY-_tF-kQveeYdyaxjWg";
-  },
+watch(locale, (newValue, oldValue) => {
+  if (newValue === "zhTW") {
+    srcID.value = "1dR9TQG2tWyi2qUItkKAettg8ZL0_S2uOHEJFs7ShDWI";
+  } else {
+    srcID.value = "1yJXBeQioa_mpX7RT10jgjr0WY-_tF-kQveeYdyaxjWg";
+  }
+  initTimeline();
+});
+
+const initTimeline = () => {
+  window.timeline = new TL.Timeline(
+    "timeline-embed",
+    `https://docs.google.com/spreadsheets/d/${srcID.value}/pubhtml`,
+    options
+  );
+};
+
+onMounted(() => {
+  if (process.client) {
+    initTimeline();
+  }
 });
 </script>
 
@@ -85,8 +80,18 @@ const srcID = computed({
 h1 {
   color: var(--color-primary);
 }
-.custom-timeline :deep(.tl-timenav) {
-  background-color: transparent;
+
+.block-title {
+  width: 100%;
+  height: auto;
+  padding: 0.5rem 1rem;
+  background-color: var(--block-title-bg);
+  color: var(--color-text);
+  margin-bottom: 0;
+}
+
+:deep(.tl-timenav) {
+  background-color: var(--main-bg);
   border: none;
 }
 </style>
